5cb25a731ace41581762f804745b05b58583289c
pauleichhorn <paei002@gmail.com>
2024-08-27 15:59:05 +0200

    add gitignore

--- flask_app.py
+++ flask_app.py
@@ -12,16 +12,15 @@ from Blueprints.add_bbox import add_bbox_blueprint
 app = Flask(__name__)
 CORS(app)  # Erlaubt Anfragen von allen Domänen
 
-
 app.register_blueprint(home_Classification_blueprint)
-if __name__ == '__main__':
-    app.run(debug=True)
 
 app.register_blueprint(add_label_blueprint)
 app.register_blueprint(add_bbox_blueprint)
 app.register_blueprint(ai_classification_blueprint)
 
-
 @app.route('/')
 def home():
     return render_template('classificator_home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5555)
